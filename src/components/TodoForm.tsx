af15f7cc103ff0f509d28ce29a9675591defe646
Patrick W Troughton <troughp@gmail.com>
2020-06-01 16:47:16 -0400

    Add-create-form

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,39 +1,49 @@
 import React from 'react';
 import {Input, Button, Form, Switch} from 'antd';
+import styles from './styles.module.css';
+import {postTodo} from "../services/todoService";
 
 const layout = {
     labelCol: {span: 8},
-    wrapperCol: {span: 16},
+    wrapperCol: {span: 8},
 };
 const tailLayout = {
-    wrapperCol: {offset: 8, span: 16},
+    wrapperCol: {offset: 8, span: 8},
 };
 
-const TodoForm = () => {
+interface TodoFormInterface {
+    reload(): void;
+}
+
+const TodoForm = (props: TodoFormInterface) => {
     const [form] = Form.useForm();
-    const onFinish = (values: any) => {
+
+    const onFinish = async (values: any) => {
         console.log(values);
+        postTodo(values).then(() => props.reload());
     };
+
     const onReset = () => {
         form.resetFields();
     };
+
     return (
         <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
-            <Form.Item name="title" label="Title" rules={[{ required: true}]}>
-                <Input />
+            <Form.Item name="title" label="Title" rules={[{required: true}]}>
+                <Input/>
             </Form.Item>
-            <Form.Item name="description" label="Description" rules={[{ required: true}]}>
-                <Input.TextArea />
+            <Form.Item name="description" label="Description" rules={[{required: true}]}>
+                <Input.TextArea/>
             </Form.Item>
             <Form.Item name="complete" label="Complete">
-                <Switch />
+                <Switch/>
             </Form.Item>
             <Form.Item {...tailLayout}>
-                <Button type="primary" htmlType="submit">
+                <Button type="primary" htmlType="submit" >
                     Submit
                 </Button>
-                <Button htmlType="button" onClick={onReset}>
-
+                <Button htmlType="button" onClick={onReset} className={styles.button}>
+                    Reset
                 </Button>
             </Form.Item>
         </Form>
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,8 +2,9 @@ import React, {Component} from "react";
 import Todo from "../services/Todo";
 import {getTodos} from "../services/todoService";
 import TodoItem from "./TodoItem";
-import {Space} from "antd";
+import {Row, Space} from "antd";
 import TodoForm from "./TodoForm";
+import styles from "./styles.module.css"
 
 interface TodoListState {
     todos: Todo[];
@@ -17,6 +18,10 @@ class TodoList extends Component<any, TodoListState> {
     }
 
     async componentDidMount() {
+        await this.loadPage();
+    }
+
+    loadPage = async () => {
         let todos = await getTodos()
         this.setState({todos, loading: false})
     }
@@ -29,13 +34,12 @@ class TodoList extends Component<any, TodoListState> {
                 <h2>this is loading</h2>
                 ) : (
                     <>
-                        <TodoForm/>
-                        <Space direction={"vertical"} style={{width: 300}}>
-
+                        <TodoForm reload={this.loadPage}/>
+                        <Row className={styles.todoRows}>
                             {this.state.todos.map((todo: Todo) =>
                                 <TodoItem key={todo.id} todo={todo}/>
                             )}
-                        </Space>
+                        </Row>
                     </>
                 )}
             </div>
